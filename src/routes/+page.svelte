5ca4f79d586cf428fc304bd85d837d5cf8e91aad
David Dal Busco <david.dalbusco@outlook.com>
2022-09-03 15:21:40 +0200

    build: sveltekit routing breaking changes (#6)

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import Canisters from '$lib/components/canisters/Canisters.svelte';
+  import Toolbar from '$lib/components/core/Toolbar.svelte';
+  import Welcome from '$lib/components/core/Welcome.svelte';
+  import {canistersEmpty} from '$lib/stores/canisters.store';
+</script>
+
+<Toolbar />
+
+<main>
+  {#if $canistersEmpty}
+    <Welcome />
+  {:else}
+    <Canisters />
+  {/if}
+</main>
