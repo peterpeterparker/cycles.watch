10d550f5311e1e56d57b8a68214d3aff7124c141
David Dal Busco <david.dalbusco@outlook.com>
2023-07-23 14:42:36 +0200

    feat: style footer

--- src/lib/components/core/Footer.svelte
+++ src/lib/components/core/Footer.svelte
@@ -1,28 +1,15 @@
+<script>
+  import IconJuno from "$lib/components/icons/IconJuno.svelte";
+</script>
+
 <footer>
-  <a
+  <a class="primary"
     href="https://juno.build"
     rel="external noopener norefferer"
     target="_blank"
     aria-label="Built with Juno">
-    <svg width="100%" height="100%" viewBox="0 0 228 49" xmlns="http://www.w3.org/2000/svg"
-      ><path
-        d="M220.294,8.921c0,-2.218 -1.8,-4.019 -4.018,-4.019l-201.552,0c-2.218,0 -4.018,1.801 -4.018,4.019l-0,34.158c-0,2.218 1.8,4.019 4.018,4.019l201.552,-0c2.218,-0 4.018,-1.801 4.018,-4.019l0,-34.158Z" /><path
-        d="M217.294,5.921c0,-2.218 -1.8,-4.019 -4.018,-4.019l-201.552,0c-2.218,0 -4.018,1.801 -4.018,4.019l-0,34.158c-0,2.218 1.8,4.019 4.018,4.019l201.552,-0c2.218,-0 4.018,-1.801 4.018,-4.019l0,-34.158Z"
-        style="fill:#fff;" /><g
-        ><text
-          x="52.982px"
-          y="29.596px"
-          style="font-family:'JetBrainsMono-Regular', 'JetBrains Mono', monospace;font-size:17.2px;"
-          >Build with Juno</text
-        ><g
-          ><path
-            d="M41.102,23c0,-6.207 -5.039,-11.246 -11.246,-11.246c-6.207,-0 -11.247,5.039 -11.247,11.246c0,6.207 5.04,11.246 11.247,11.246c6.207,0 11.246,-5.039 11.246,-11.246Z" /><circle
-            cx="39.655"
-            cy="18.708"
-            r="6.954"
-            style="fill:var(--color-primary);" /></g
-        ></g
-      ></svg>
+    <IconJuno />
+    Build with Juno
   </a>
 </footer>
 
@@ -38,6 +25,7 @@
   }
 
   a {
-    max-width: 200px;
+    display: flex;
+    gap: .45rem;
   }
 </style>
