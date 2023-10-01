19d279a03e6d2c4a609ce9290f0a1d74b4e2ba60
David Dal Busco <david.dalbusco@outlook.com>
2023-10-01 09:14:42 +0200

    feat: remove analytics

--- src/lib/services/init.services.ts
+++ src/lib/services/init.services.ts
@@ -1,4 +1,3 @@
-import {initOrbiter} from '@junobuild/analytics';
 import {initJuno as initJunoLib} from '@junobuild/core';
 
 export const initJuno = async () => {
@@ -6,21 +5,5 @@ export const initJuno = async () => {
 
   await initJunoLib({
     satelliteId: SATELLITE_ID
-  });
-};
-
-export const initAnalytics = async () => {
-  const DEV = import.meta.env.DEV;
-
-  if (DEV) {
-    return;
-  }
-
-  const SATELLITE_ID = import.meta.env.VITE_JUNO_SATELLITE_ID;
-  const ORBITER_ID = import.meta.env.VITE_JUNO_ORBITER_ID;
-
-  await initOrbiter({
-    satelliteId: SATELLITE_ID,
-    orbiterId: ORBITER_ID
   });
 };
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,14 +12,10 @@
   import Add from '$lib/components/overlays/Add.svelte';
   import {onMount} from 'svelte';
   import Busy from '$lib/components/ui/Busy.svelte';
-  import {initAnalytics, initJuno} from "$lib/services/init.services";
+  import {initJuno} from "$lib/services/init.services";
 
   onMount(
-    async () =>
-      await Promise.all([
-        initJuno(),
-        initAnalytics()
-      ])
+    async () => initJuno()
   );
 </script>
 
