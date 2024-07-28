d37921861985af79c199e4edcd06c8d6264a217d
David Dal Busco <david.dalbusco@outlook.com>
2024-07-28 11:10:53 +0200

    feat: settings

--- juno.config.mjs
+++ juno.config.mjs
@@ -4,6 +4,10 @@ import { defineConfig } from '@junobuild/config';
 export default defineConfig({
 	satellite: {
 		id: 'ck4tp-3iaaa-aaaal-ab7da-cai',
-		source: 'build'
+		source: 'build',
+		settings: {
+			heapMemoryLimit: 1073741824n,
+			logVisibility: "controllers"
+		}
 	}
 });
