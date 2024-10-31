283798584084fa8fc52defb26146479595adb8c7
David Dal Busco <david.dalbusco@outlook.com>
2024-10-31 10:04:07 +0100

    feat: deploy with build and update config ts

---
+++ juno.config.ts
@@ -0,0 +1,13 @@
+import {defineConfig} from '@junobuild/config';
+
+export default defineConfig({
+  satellite: {
+    id: 'ck4tp-3iaaa-aaaal-ab7da-cai',
+    source: 'build',
+    settings: {
+      heapMemoryLimit: 1073741824n,
+      logVisibility: 'controllers'
+    },
+    predeploy: ['npm run build']
+  }
+});
