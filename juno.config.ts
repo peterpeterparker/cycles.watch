e3b8ca796cb9633f8f35aeb401a1f731f37f911b
David Dal Busco <david.dalbusco@outlook.com>
2024-12-09 06:29:01 +0100

    chore: fmt

--- juno.config.ts
+++ juno.config.ts
@@ -1,13 +1,13 @@
-import {defineConfig} from '@junobuild/config';
+import { defineConfig } from '@junobuild/config';
 
 export default defineConfig({
-  satellite: {
-    id: 'ck4tp-3iaaa-aaaal-ab7da-cai',
-    source: 'build',
-    settings: {
-      heapMemoryLimit: 1073741824n,
-      logVisibility: 'controllers'
-    },
-    predeploy: ['npm run build']
-  }
+	satellite: {
+		id: 'ck4tp-3iaaa-aaaal-ab7da-cai',
+		source: 'build',
+		settings: {
+			heapMemoryLimit: 1073741824n,
+			logVisibility: 'controllers'
+		},
+		predeploy: ['npm run build']
+	}
 });
