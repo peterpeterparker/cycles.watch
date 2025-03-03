4ae2f0984e07d4ac7a177d7b7b7b88b56f6614d1
David Dal Busco <david.dalbusco@outlook.com>
2025-03-03 15:49:35 +0100

    feat: transfer from

--- src/satellite/src/ledger.rs
+++ src/satellite/src/ledger.rs
@@ -1,12 +1,33 @@
 use candid::{Nat, Principal};
 use ic_cdk::api::call::CallResult;
-use ic_cdk::call;
+use ic_cdk::{call};
 use icrc_ledger_types::icrc1::account::Account;
+use icrc_ledger_types::icrc2::transfer_from::{TransferFromArgs, TransferFromError};
 
 pub async fn icrc_balance_of(
     ledger_id: Principal,
-    account: Account
+    account: &Account
 ) -> CallResult<Nat> {
     let (result,): (Nat,) = call(ledger_id, "icrc1_balance_of", (account,)).await?;
+    Ok(result)
+}
+
+pub async fn icrc_transfer_from(
+    ledger_id: Principal,
+    from_account: &Account,
+    to_account: &Account,
+    icp_amount: &Nat,
+) -> CallResult<Result<Nat, TransferFromError>> {
+    let args: TransferFromArgs = TransferFromArgs {
+        amount: icp_amount.clone(),
+        from: from_account.clone(),
+        to: to_account.clone(),
+        created_at_time: None,
+        fee: None,
+        memo: None,
+        spender_subaccount: None
+    };
+
+    let (result,): (Result<Nat, TransferFromError>,) = call(ledger_id, "icrc2_transfer_from", (args,)).await?;
     Ok(result)
 }
--- src/satellite/src/lib.rs
+++ src/satellite/src/lib.rs
@@ -2,14 +2,14 @@ mod types;
 mod ledger;
 mod env;
 
-use candid::Principal;
-use ic_cdk::{print, trap};
+use candid::{Nat, Principal};
+use ic_cdk::{id, print, trap};
 use icrc_ledger_types::icrc1::account::Account;
 use junobuild_macros::on_set_doc;
 use junobuild_satellite::{include_satellite, OnSetDocContext};
 use junobuild_utils::decode_doc_data;
 use crate::env::ICP_LEDGER_ID;
-use crate::ledger::icrc_balance_of;
+use crate::ledger::{icrc_balance_of, icrc_transfer_from};
 use crate::types::RequestData;
 
 #[on_set_doc(collections = ["request"])]
@@ -18,14 +18,35 @@ async fn on_set_doc(context: OnSetDocContext) -> Result<(), String> {
 
     print(format!("There is a new swap request from {} for {:?}", data.wallet_owner.value.to_text(), data.icp_amount.value));
 
-    let account: Account = Account::from(data.wallet_owner.value);
+    let from_account: Account = Account::from(data.wallet_owner.value);
 
-    let balance = icrc_balance_of(Principal::from_text(ICP_LEDGER_ID).unwrap(), account)
+    let balance = icrc_balance_of(Principal::from_text(ICP_LEDGER_ID).unwrap(), &from_account)
         .await
-        .map_err(|e| trap(&format!("Failed to call ICRC ledger: {:?}", e)))
+        .map_err(|e| trap(&format!("Failed to call ICRC ledger icrc_balance_of: {:?}", e)))
         .map_err(|e| trap(&format!("Failed to get the balance: {:?}", e)));
 
+    // TODO: if balance < icp_amount => error
+
     print(format!("Balance of the caller is {:?}", balance));
+
+    // TODO: `?!?!?!
+    let smaller_amount = data.icp_amount.value - 10_000;
+
+    let to_account: Account = Account::from(id());
+
+    let result = icrc_transfer_from(Principal::from_text(ICP_LEDGER_ID).unwrap(), &from_account, &to_account, &Nat::from(smaller_amount))
+        .await
+        .map_err(|e| trap(&format!("Failed to call ICRC ledger icrc_transfer_from: {:?}", e)))
+        .map_err(|e| trap(&format!("Failed to execute the transfer from: {:?}", e)));
+
+    print(format!("Result of the transfer from is {:?}", result));
+
+    let satellite_balance = icrc_balance_of(Principal::from_text(ICP_LEDGER_ID).unwrap(), &to_account)
+        .await
+        .map_err(|e| trap(&format!("Failed to call ICRC ledger icrc_balance_of: {:?}", e)))
+        .map_err(|e| trap(&format!("Failed to get the balance: {:?}", e)));
+
+    print(format!("This is now the balance of the satellite {:?}", satellite_balance));
 
     // TODO: implement ICP -> cycles swap
     // keep track of the requests
