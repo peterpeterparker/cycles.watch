30bb3dbea4b51317ca052e62338772b5678a116f
David Dal Busco <david.dalbusco@outlook.com>
2025-04-07 12:40:09 +0200

    feat: use skylab

--- juno.config.mjs
+++ juno.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from '@junobuild/config';
 export default defineConfig({
 	satellite: {
 		ids: {
-			development: 'aovwi-4maaa-aaaaa-qaagq-cai',
 			production: 'ck4tp-3iaaa-aaaal-ab7da-cai'
 		},
 		source: 'build',
