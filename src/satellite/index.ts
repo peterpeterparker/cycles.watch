b54ae6fd6c7aed30984bad80bbe10fd1c278c670
David Dal Busco <david.dalbusco@outlook.com>
2025-04-20 12:26:21 +0200

    feat: book keep error

--- src/satellite/index.ts
+++ src/satellite/index.ts
@@ -1,17 +1,14 @@
-import { type RequestData, RequestDataSchema } from '$lib/types/datastore';
-import type { Account } from '@dfinity/ledger-icrc/dist/candid/icrc_ledger';
-import { type AssertSetDoc, defineAssert, defineHook, type OnSetDoc } from '@junobuild/functions';
-import { id } from '@junobuild/functions/ic-cdk';
-import { decodeDocData } from '@junobuild/functions/sdk';
-import { icrcBalanceOf } from './api/ledger-icrc.api';
-import { ICP_LEDGER_ID } from './constants/functions.constants';
+import { RequestDataSchema } from '$lib/types/datastore';
 import {
-	saveIcpToCyclesSwapped,
-	saveIcpTransferredFromWallet,
-	saveIcpTransferredToCmc
-} from './services/bookkeeping.services';
-import { notifyTopUp, transferIcpToCmc } from './services/cmc.services';
-import { assertWalletBalance, transferIcpFromWallet } from './services/wallet.services';
+	type AssertSetDoc,
+	defineAssert,
+	defineHook,
+	type OnSetDoc,
+	type OnSetDocContext,
+	type RunFunction
+} from '@junobuild/functions';
+import { decodeDocData } from '@junobuild/functions/sdk';
+import { swapIcpToCycles } from './services/swap.services';
 
 export const assertSetDoc = defineAssert<AssertSetDoc>({
 	collections: ['request'],
@@ -21,106 +18,17 @@ export const assertSetDoc = defineAssert<AssertSetDoc>({
 	}
 });
 
+const collections = ['request'] as const;
+
+type OnSetDocCollection = (typeof collections)[number];
+
 export const onSetDoc = defineHook<OnSetDoc>({
-	collections: ['request'],
+	collections,
 	run: async (context) => {
-		// ###############
-		// Init data
-		// ###############
-
-		const requestKey = context.data.key;
-
-		const data = decodeDocData<RequestData>(context.data.data.after.data);
-
-		const fromAccount: Account = {
-			owner: data.wallet_owner,
-			subaccount: []
+		const fn: Record<OnSetDocCollection, RunFunction<OnSetDocContext>> = {
+			request: swapIcpToCycles
 		};
 
-		const {
-			swap: { amount: requestAmount, fee },
-			target_canister_id: targetCanisterId
-		} = data;
-
-		const ledgerId = ICP_LEDGER_ID;
-
-		// ###############
-		// Check current account balance. This way the process can stop early on
-		// ###############
-		await assertWalletBalance({
-			ledgerId,
-			fromAccount,
-			amount: requestAmount,
-			fee
-		});
-
-		// ###############
-		// Transfer from wallet to satellite.
-		// ###############
-
-		const toAccount: Account = {
-			owner: id(),
-			subaccount: []
-		};
-
-		const blockIndexWallet = await transferIcpFromWallet({
-			ledgerId,
-			fromAccount,
-			toAccount,
-			amount: requestAmount,
-			fee
-		});
-
-		// ###############
-		// We keep an internal track of the transferred ICP from the wallet
-		// ###############
-
-		saveIcpTransferredFromWallet({
-			requestKey,
-			blockIndex: blockIndexWallet
-		});
-
-		// ###############
-		// Use ICP to topup the targeted canister with the provided ICP
-		// ###############
-
-		const blockIndex = await transferIcpToCmc({
-			ledgerId,
-			targetCanisterId,
-			amount: requestAmount
-		});
-
-		// We keep an internal track of the transferred ICP to the CMC
-		saveIcpTransferredToCmc({
-			requestKey,
-			blockIndex
-		});
-
-		const cycles = await notifyTopUp({
-			blockIndex,
-			targetCanisterId
-		});
-
-		// ###############
-		// We keep an internal track of the success swap.
-		// ###############
-
-		saveIcpToCyclesSwapped({
-			requestKey,
-			cycles
-		});
-
-		// ###############
-		// Just a print out to check the balance while developing.
-		// ###############
-
-		const balance = await icrcBalanceOf({
-			ledgerId,
-			account: toAccount
-		});
-
-		console.log('This is now the balance of the satellite:', balance);
-
-		// TODO: save errors
+		await fn[context.data.collection as OnSetDocCollection]?.(context);
 	}
 });
