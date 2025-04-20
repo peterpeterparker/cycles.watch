369dd2545f94da32d7e4b1b3db025f9519049dbb
David Dal Busco <david.dalbusco@outlook.com>
2025-04-20 09:18:16 +0200

    feat: assert total

--- src/satellite/services/wallet.services.ts
+++ src/satellite/services/wallet.services.ts
@@ -1,9 +1,13 @@
+import { encodeIcrcAccount } from '@dfinity/ledger-icrc';
 import type { Account } from '@dfinity/ledger-icrc/dist/candid/icrc_ledger';
+import { fromNullable } from '@dfinity/utils';
 import { icrcBalanceOf, icrcTransferFrom } from '../api/ledger-icrc.api';
 
 export const assertWalletBalance = async ({
 	ledgerId,
-	fromAccount
+	fromAccount,
+	amount,
+	fee
 }: {
 	ledgerId: string;
 	fromAccount: Account;
@@ -15,7 +19,18 @@ export const assertWalletBalance = async ({
 		account: fromAccount
 	});
 
-	console.log('Balance ->', balance);
+	const total = amount + (fee ?? 10_000n);
+
+	if (balance < total) {
+		const encodedAccountText = encodeIcrcAccount({
+			owner: fromAccount.owner,
+			subaccount: fromNullable(fromAccount.subaccount)
+		});
+
+		throw new Error(
+			`Balance ${balance} is smaller than ${total} for account ${encodedAccountText}.`
+		);
+	}
 };
 
 export const transferIcpFromWallet = async (params: {
