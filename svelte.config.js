60cbeb28347e0dfea3ea05142e2d7ecf7515ba4d
David Dal Busco <david.dalbusco@outlook.com>
2025-04-20 14:04:39 +0200

    build: reproducibility

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,11 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { readFileSync } from 'node:fs';
+import { fileURLToPath } from 'node:url';
+
+const file = fileURLToPath(new URL('package.json', import.meta.url));
+const json = readFileSync(file, 'utf8');
+const { version } = JSON.parse(json);
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,8 +15,13 @@ const config = {
 
 	kit: {
 		adapter: adapter(),
+
 		serviceWorker: {
 			register: false
+		},
+
+		version: {
+			name: version
 		}
 	}
 };
