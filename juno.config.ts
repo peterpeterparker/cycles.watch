cf8cda167c1078c18d3d49efb1a8d37b39725be2
David Dal Busco <david.dalbusco@outlook.com>
2025-08-06 07:41:12 +0200

    chore: local dev

--- juno.config.ts
+++ juno.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from '@junobuild/config';
 export default defineConfig({
 	satellite: {
 		ids: {
-			development: 'be2us-64aaa-aaaaa-qaabq-cai',
+			development: 'atbka-rp777-77775-aaaaq-cai',
 			production: 'ck4tp-3iaaa-aaaal-ab7da-cai'
 		},
 		source: 'build',
