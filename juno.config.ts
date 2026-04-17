dfb9b7bee37d6b045bd91b18e73eeb084cfa05a9
David Dal Busco <david.dalbusco@outlook.com>
2026-04-17 09:20:28 +0200

    chore: hosting instead of flat juno config (#62)

--- juno.config.ts
+++ juno.config.ts
@@ -42,24 +42,26 @@ export default defineConfig({
 			development: 'atbka-rp777-77775-aaaaq-cai',
 			production: 'ck4tp-3iaaa-aaaal-ab7da-cai'
 		},
-		source: 'build',
+		hosting: {
+			source: 'build',
+			predeploy: ['npm run build'],
+			precompress: [
+				{
+					pattern: '**/*.+(js|mjs|css)',
+					algorithm: 'brotli',
+					mode: 'replace'
+				},
+				{
+					pattern: '**/*.html',
+					algorithm: 'brotli',
+					mode: 'both'
+				}
+			]
+		},
 		settings: {
 			heapMemoryLimit: 1073741824n,
 			logVisibility: 'controllers'
 		},
-		predeploy: ['npm run build'],
-		precompress: [
-			{
-				pattern: '**/*.+(js|mjs|css)',
-				algorithm: 'brotli',
-				mode: 'replace'
-			},
-			{
-				pattern: '**/*.html',
-				algorithm: 'brotli',
-				mode: 'both'
-			}
-		],
 		assertions: {
 			heapMemory: 800_000_000n
 		},
